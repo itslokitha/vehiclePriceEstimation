2d549473e502e49c64fd7e2a3515218665e06467
itslokitha <anu99nilaweera@gmail.com>
2023-10-03 22:48:00 -0300

    dataTransformation file changes

--- dataTransformation.py
+++ dataTransformation.py
@@ -1,26 +1,18 @@
+import os
 import pandas as pd
 
-def get_average_value():
-    year = input("Enter the year of the vehicle: ")
-    make = input("Enter the make of the vehicle: ")
-    model = input("Enter the model of the vehicle: ")
-    odometer = int(input("Enter the odometer reading of the vehicle: "))
+csv_directory = '/Users/lokitha/Desktop/Final Project/dataExtractionProgram/datasheets'
 
-    file_path = '/Users/lokitha/Desktop/Final Project/dataExtractionProgram/datasheets/kijiji_data_2023-10-03.csv'  # Adjust the file path as needed
+master_df = pd.DataFrame()
 
-    try:
-        car_df = pd.read_csv(file_path)
-        filtered_df = car_df[(car_df['model_year'] == int(year)) & 
-                             (car_df['brand'] == make) & 
-                             (car_df['model'] == model)]
+for filename in os.listdir(csv_directory):
+    if filename.endswith('.csv'):
+
+        file_path = os.path.join(csv_directory, filename)
+        df = pd.read_csv(file_path)
         
-        if not filtered_df.empty:
-            avg_price = filtered_df['list_price'].mean()
-            print(f"The average price for a {year} {make} {model} with {odometer} miles is: ${avg_price:.2f}")
-        else:
-            print(f"No data found for {year} {make} {model}.")
+        df_cleaned = df.drop_duplicates(subset=['model_year', 'brand', 'model', 'mileage', 'color', 'list_price'], keep='last')
+        
+        master_df = pd.concat([master_df, df_cleaned], ignore_index=True)
 
-    except FileNotFoundError:
-        print(f"File '{file_path}' not found.")
-
-get_average_value()
+master_df.to_csv('master_data.csv', index=False)
