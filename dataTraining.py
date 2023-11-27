9022643eef369c09e854e5fa98d7b31acd598973
itslokitha <anu99nilaweera@gmail.com>
2023-11-26 23:01:36 -0400

    updating the data model

--- dataTraining.py
+++ dataTraining.py
@@ -2,17 +2,28 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from flask import Flask, jsonify, request
+from datetime import datetime
 
 app = Flask(__name__)
 
 file_path = 'master_data.csv'
 master_data = pd.read_csv(file_path)
 
-features = master_data[['brand', 'model', 'model_year', 'mileage']].dropna()
+# Convert 'model_year' to an integer if it's not already
+master_data['model_year'] = pd.to_numeric(master_data['model_year'], errors='coerce').fillna(0).astype(int)
+
+# Calculate vehicle age from model year
+current_year = datetime.now().year
+master_data['vehicle_age'] = current_year - master_data['model_year']
+
+features = master_data[['brand', 'model', 'vehicle_age', 'mileage']].dropna()
 target = master_data['list_price'][features.index]
 
 # One-hot encode categorical features
-features_encoded = pd.get_dummies(features)
+features_encoded = pd.get_dummies(features[['brand', 'model']])
+
+features_encoded['vehicle_age'] = features['vehicle_age']
+features_encoded['mileage'] = features['mileage']
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(features_encoded, target, test_size=0.2, random_state=42)
@@ -22,12 +33,12 @@ rf_model = RandomForestRegressor(n_estimators=100, random_state=42)
 rf_model.fit(X_train, y_train)
 
 # Function to predict the price
-def predict_price(model_year, brand, model, mileage, trained_model, training_features):
+def predict_price(vehicle_age, brand, model, mileage, trained_model, training_features):
     # Initialize input data with zeros for all features
     input_data = pd.DataFrame(columns=training_features.columns)
     input_data.loc[0] = 0
     # Set the values for the input data
-    input_data['model_year'] = model_year
+    input_data['vehicle_age'] = vehicle_age
     input_data['mileage'] = mileage
     input_data[f'brand_{brand}'] = 1
     input_data[f'model_{model}'] = 1
@@ -36,6 +47,7 @@ def predict_price(model_year, brand, model, mileage, trained_model, training_fea
     
     predicted_price_rounded = round(predicted_price[0], 2)
     return predicted_price_rounded
+
 
 # Flask web server routes (when running locally, change the route of the static_folder)
 app = Flask(__name__, static_url_path='', static_folder='website')
