ee4aa9b61e8b402d407f8439ce773c500ab5486f
itslokitha <anu99nilaweera@gmail.com>
2023-11-29 18:33:58 -0400

    updating header and footer

--- website/script.js
+++ website/script.js
@@ -206,3 +206,29 @@ document.getElementById('theme-toggle').addEventListener('change', function(even
         label.textContent = 'Dark Mode';
     }
 });
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4 && this.status == 200) {
+                    elmnt.innerHTML = this.responseText;
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }      
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
+
+document.getElementById("header-placeholder").setAttribute("w3-include-html", "header.html");
+document.getElementById("footer-placeholder").setAttribute("w3-include-html", "footer.html");
+includeHTML();
